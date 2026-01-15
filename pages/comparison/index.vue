9921a89d8f05847ef58264791ce0f038fc852c22
Alexander <rubick12545@gmail.com>
2026-01-15 14:41:35 +0200

    fix

--- pages/comparison/index.vue
+++ pages/comparison/index.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import { PublicRoutes } from "~/enums/routes.enum";
 import { useCompStore } from "~/stores/comparisonStore";
 
 const compStore = useCompStore();
 </script>
 
 <template>
-  <div class="flex flex-col xl:w-full xl:max-w-277.5 w-11/12">
+  <div
+    class="flex flex-col xl:w-full xl:max-w-277.5 w-11/12"
+    v-if="compStore.allList.length > 0"
+  >
     <template
       v-for="(category, index) in compStore.allList"
       :key="category.category"
@@ -16,5 +20,13 @@ const compStore = useCompStore();
         class="bg-accent-100 w-full h-px my-2.5"
       ></div>
     </template>
+  </div>
+  <div v-else class="flex flex-col items-center text-2xl font-semibold">
+    <p>Немає товарів для порівняння</p>
+    <NuxtLink
+      :to="PublicRoutes.CATALOG"
+      class="border-b-2 border-b-transparent hover:border-b-main-400 duration-300"
+      >До каталогу</NuxtLink
+    >
   </div>
 </template>
